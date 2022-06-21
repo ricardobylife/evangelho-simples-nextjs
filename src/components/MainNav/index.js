befc3b6c9fea8667b659e7f47aa76feff74c4aa3
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-06-21 10:48:15 -0300

    animation adjustments and comments

--- src/components/MainNav/index.js
+++ src/components/MainNav/index.js
@@ -13,10 +13,11 @@ const NavMain = styled.nav`
     margin-top: -20px;
     margin-bottom: -17px;
   }
+  //animation main menu
   li:hover {
-    font-size: clamp(1.3em, 9vw, 3.5em);
+    font-size: clamp(2.7em, 0.6em + 3.8vw, 3.5em);
 
-    transition: all 0.3s ease-in-out;
+    transition: all 0.4s ease-in-out;
   }
   /* @media screen and (max-width: 768px) {
     li {
@@ -34,12 +35,20 @@ const NavMain = styled.nav`
       font-size: clamp(1em, 11vw, 3.2em);
     }
   } */
+
+  //reajustment in main menu for samll screens
   @media screen and (max-width: 425px) {
     ul {
       text-align: center;
     }
     li {
       padding-top: 0.1em;
+      font-size: clamp(2.2em, 0.6em + 3.5vw, 3.5em);
+    }
+    li:hover {
+      font-size: clamp(2.4em, 0.6em + 3.8vw, 3.5em);
+
+      transition: all 0.3s ease-in-out;
     }
   }
 `;
